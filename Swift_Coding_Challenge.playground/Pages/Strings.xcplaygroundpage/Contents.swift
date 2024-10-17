495ddfe53abb593abbca616d431d555ace08095a
Habibur Rahman <mhr.habibur@gmail.com>
2024-10-17 06:42:36 +0600

    Rename and code refactor

--- Swift_Coding_Challenge.playground/Pages/Strings.xcplaygroundpage/Contents.swift
+++ Swift_Coding_Challenge.playground/Pages/Strings.xcplaygroundpage/Contents.swift
@@ -1,7 +1,39 @@
-
 import Foundation
 
+// MARK: - Challenge 1: Are the letters unique?
 
-// MARK: Strings
+"""
+
+func isUnique(_ input: String) -> Bool {
+    let lowercasedInput = input.lowercased()
+    return Set(lowercasedInput).count == lowercasedInput.count
+}
+
+print(isUnique("abca"))
+
+"""
+
+
+// MARK: - Challenge 2: Is a string a palindrome?
+
+"""
+
+func isPalindrome(_ input: String) -> Bool {
+    return input == String(input.reversed())
+}
+
+print(isPalindrome("racecar"))
+
+
+"""
+
+//func isPalindrome(_ input: String) -> Bool {
+//    return input == String(input.reversed())
+//}
+
+func isUnique(_ input: String) -> Bool {
+    let lowercasedInput = input.lowercased()
+    return Set(lowercasedInput).count == lowercasedInput.count
+}
 
 
